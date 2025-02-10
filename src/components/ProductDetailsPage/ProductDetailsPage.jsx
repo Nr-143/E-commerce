53105bc0253fac0c6d9b382b2faf65a0ccd92c48
Klodev <NIrmalraj@klodev.com>
2025-02-10 21:56:55 +0530

    Prodduct details fetch api written

--- src/components/ProductDetailsPage/ProductDetailsPage.jsx
+++ src/components/ProductDetailsPage/ProductDetailsPage.jsx
@@ -35,6 +35,11 @@ const ProductDetailsPage = () => {
     const fetchProductDetails = async () => {
       setLoading(true);
       setProduct(null);
+ const response = await fetch(
+   `https://world.openfoodfacts.org/api/v0/product/${3017620422003}.json`
+ );
+      const data = await response.json();
+   
       try {
         const productData = {
           id: 101,
